1d22c4ca57bf37cdeb5d47b19309b7590d52c1f4
Lucas <kimlucas92@gmail.com>
2020-06-25 16:53:22 -0300

    plot1 styling

--- dataManipulation/dataManipulation.py
+++ dataManipulation/dataManipulation.py
@@ -8,36 +8,44 @@ import seaborn as sns
 earningsHistory = pd.read_csv('../scrapy/earningsHistory/eSports_players.csv')
 earningsCountry = pd.read_csv(
     '../scrapy/earningsByCountry/earningsCountry_players.csv')
+countryEarnings = pd.read_csv('../scrapy/countryEarnings/countryEarnings.csv')
 
-
+#=============================================================================#
 # plot1 - countries ranking by total earnings
-sns.set(style="whitegrid")
-f, ax = plt.subplots(figsize=(6, 15))
-
-countriesRankingEarnings_df = earningsHistory[['country', 'totalYear']]
+# ---------prepare data
+countriesRankingEarnings_df = countryEarnings[['country', 'totalOverall']]
 countriesRankingEarnings = countriesRankingEarnings_df.groupby(
-    'country', as_index=False).sum().sort_values('totalYear', ascending=False).head(20)
+    'country', as_index=False).sum().sort_values('totalOverall', ascending=False).head(20)
+
+# ---------plot
+sns.set(style="whitegrid")
+countriesRankingEarnings_plot = sns.barplot(
+    x="totalOverall", y="country", data=countriesRankingEarnings, color='g')
+plt.xlabel('Purchase amount', fontsize=18)
+countriesRankingEarnings_plot.set(xlabel='Total Earnings in US$', ylabel='')
 
 
-sns.set_color_codes("pastel")
-sns.barplot(x="totalYear", y="country",
-            data=countriesRankingEarnings, label="Total", color="b")
-
-
+#=============================================================================#
 # plot2 - eSports earnings by year
 plt.figure(figsize=(12, 6))
 earningsHistory.groupby('year')['totalYear'].sum().sort_values(
     ascending=True).plot.bar(color='r')
 
+
+#=============================================================================#
 # plot3 - most successful players
 plt.figure(figsize=(12, 6))
 earningsCountry.sort_values('totalOverall', ascending=False).head(10)
 
+
+#=============================================================================#
 # plot4 - games rank by prize money
 plt.figure(figsize=(12, 6))
 earningsCountry.groupby('game')['totalOverall'].sum().sort_values(
     ascending=False).head(20).plot.bar(color='r')
 
+
+#=============================================================================#
 # plot5 - top game by year
 plt.figure(figsize=(12, 6))
 earningsHistory.groupby('year')['totalOverall', 'game']
