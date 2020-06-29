3e965b992f0f572a443dda42b89b03ad3257fe10
Lucas <kimlucas92@gmail.com>
2020-06-29 18:46:57 -0300

    update webapp images

--- dataManipulation/dataManipulation.py
+++ dataManipulation/dataManipulation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 import numpy
+import squarify
 
 
 # import data sets
@@ -13,6 +14,11 @@ earningsCountry = pd.read_csv(
 countryEarnings = pd.read_csv('../scrapy/countryEarnings/countryEarnings.csv')
 femalePlayers = pd.read_csv('../scrapy/femalePlayers/femalePlayers.csv')
 earningsU18 = pd.read_csv('../scrapy/earningsU18/U18_players.csv')
+gamesRankings = pd.read_csv('../scrapy/gameRankings/gameRankings.csv')
+
+# plots fonts
+osaka = {'fontname': 'Osaka'}
+
 
 #=============================================================================#
 # plot1 - countries ranking by total earnings
@@ -40,31 +46,54 @@ earningsHistory.groupby('year')['totalYear'].sum().sort_values(
 
 #=============================================================================#
 # plot3 - most successful players
-plt.figure(figsize=(20, 6))
+plt.figure(figsize=(16, 6))
 topPlayers_df = earningsCountry.sort_values(
-    'totalOverall', ascending=False).head(15)
+    'totalOverall', ascending=False).head(16)
 
 topPlayers_plot = plt.bar(
-    topPlayers_df['playerID'], topPlayers_df['totalOverall'])
-plt.xlabel('Player in-game ID')
-plt.ylabel('Earnings US$')
+    topPlayers_df['playerID'], topPlayers_df['totalOverall']/1e6, color='#1a2139')
+plt.title('Top players earnings all time', ** osaka)
+plt.ylabel('Millions US$', **osaka)
+plt.grid(b=None, axis='x')
 
 plt.savefig('../shiny/www/images/topPlayers.png')
 
 
 #=============================================================================#
 # plot4 - games rank by prize money
-plt.figure(figsize=(12, 6))
-earningsCountry.groupby('game')['totalOverall'].sum().sort_values(
-    ascending=False).head(20).plot.bar(color='r')
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 10), tight_layout=True)
 
-plt.savefig('../shiny/www/images/topGames_prizepool.png')
+gr_df = gamesRankings.sort_values('totalOverall', ascending=False)
+genresR_df = gamesRankings.groupby('genre').sum(
+).sort_values('totalOverall', ascending=False).reset_index()
+# some games names are too long, need to shorten them
+gr_df.loc[gr_df['game'] == 'PLAYERUNKNOWN’S BATTLEGROUNDS', 'game'] = 'PUBG'
+gr_df.loc[gr_df['game'] == 'Counter-Strike: Global Offensive', 'game'] = 'CS:GO'
+gr_df.loc[gr_df['game'] == 'Heroes of the Storm', 'game'] = 'HOTS'
+gr_df.loc[gr_df['game'] == 'Arena of Valor', 'game'] = 'AoV'
+
+color = ['#f73a18', '#C72508', '#AD351F', '#BD4631',
+         '#A64230', '#F05F46', '#AB5141', '#96230F', '#993D2C', '#CF523C']
+color2 = ['#1a2139', '#7B839E', '#183185', '#040A1C',
+          '#688AFC', '#717582', '#223D94', '#575E75', '#2154FC', '#233982']
+
+plt.subplot(1, 2, 1)
+squarify.plot(sizes=gr_df.totalOverall.head(
+    10), label=gr_df.game.head(10), color=color, text_kwargs={'fontsize': 15, 'color': 'white'})
+plt.title('Game titles by prize pool', ** osaka)
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+squarify.plot(sizes=genresR_df.totalOverall.head(
+    10), label=genresR_df.genre.head(10), color=color2, text_kwargs={'fontsize': 15, 'color': 'white'})
+plt.title('Genres by prize pool', ** osaka)
+plt.axis('off')
+
+plt.savefig('../shiny/www/images/gamesgenresPrizePool.png')
 
 
 #=============================================================================#
-# plot5 - top game by year
-plt.figure(figsize=(12, 6))
-earningsHistory.groupby('year')['totalOverall', 'game']
+# plot5 - genres rank by prize money
 
 
 #=============================================================================#
@@ -99,9 +128,22 @@ U18earnings_df['totalOver18'] = U18earnings_df['totalOverall'] - \
     U18earnings_df['totalU18']
 
 # ---------plot
-plt.figure(figsize=(12, 6))
-plt.bar(U18earnings_df.playerID.head(10), U18earnings_df.totalU18.head(10))
-plt.bar(U18earnings_df.playerID.head(10), U18earnings_df.totalOver18.head(10),
-        bottom=U18earnings_df.totalU18.head(10))
+plt.figure(figsize=(16, 6))
+
+p1 = plt.bar(U18earnings_df.playerID.head(
+    15), U18earnings_df.totalU18.head(15)/1e6, color='#f73a18')
+p2 = plt.bar(U18earnings_df.playerID.head(15), U18earnings_df.totalOver18.head(15)/1e6,
+             bottom=U18earnings_df.totalU18.head(15)/1e6, color='#1a2139')
+plt.title('Top players with earnings under 18 years of age', ** osaka)
+plt.ylabel('Millions US$', **osaka)
+plt.xticks(U18earnings_df.playerID.head(15), **osaka)
+plt.legend((p1[0], p2[0]), ('Under 18', 'Over 18'))
+plt.grid(b=None, axis='x')
 
 plt.savefig('../shiny/www/images/U18_earnings.png')
+
+
+#=============================================================================#
+# plot9 - top game by year
+plt.figure(figsize=(12, 6))
+earningsHistory.groupby('year')['totalOverall', 'game']
