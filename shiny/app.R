e006e12179ac2baaa4e92cd5facde33dce9d643a
Lucas <kimlucas92@gmail.com>
2020-07-02 01:51:49 -0300

    polishing and organizing webapp

--- shiny/app.R
+++ shiny/app.R
@@ -3,6 +3,12 @@ server <- function(input, output) {
     gvisGeoChart(plot1_1, locationvar="country", colorvar="numberPlayers",
                  options=list(width=800, height=600, colorAxis="{colors:['white', '#f73a18']}"))
   })
+  
+  output$plot1_1 <- renderGvis({
+    gvisGeoChart(plot1_1, locationvar="country", colorvar="totalOverall",
+                 options=list(width=800, height=600, colorAxis="{colors:['white', '#1a2139']}"))
+  })
+  
 }
 
 shinyApp(ui = htmlTemplate("www/index.html"), server)
